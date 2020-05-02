9973c750dc42395feaa0675418ac9f55470d5ef9
lazymushroom22 <33084619+lazymushroom22@users.noreply.github.com>
2020-05-02 18:57:47 +0300

    Add files via upload

---
+++ CarRentalProject/CarRentalProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CarRentalProject;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CarRentalProject.Areas.Identity.IdentityHostingStartup))]
+namespace CarRentalProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
