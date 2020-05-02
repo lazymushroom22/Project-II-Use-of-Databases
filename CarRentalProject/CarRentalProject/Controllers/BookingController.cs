7ccaa9ac6b6a76dfa75c43ee2879adbef8c66f68
andreeapirvulescub <62211181+andreeapirvulescub@users.noreply.github.com>
2020-05-02 03:02:03 +0300

    Add files via upload

---
+++ CarRentalProject/CarRentalProject/Controllers/BookingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRentalProject.ApplicationLogic.Abstractions;
+using CarRentalProject.ApplicationLogic.DataModel;
+using CarRentalProject.ApplicationLogic.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalProject.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly iBookingRepository bookingRepository;
+
+        public BookingController(iBookingRepository bookingRepository)
+        {
+            this.bookingRepository = bookingRepository;
+        }
+
+        public ActionResult GetMachines()
+        {
+            Guid carIdGuid = Guid.Empty;
+            Car carGuid = null;
+            var cardetails = bookingRepository.GetCarDetails(carIdGuid, carGuid);
+            return View(cardetails);
+        }
+
+    }
+}
