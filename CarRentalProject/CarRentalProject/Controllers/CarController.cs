5a9914648fd6af1ba717ad85743deafc1d34b332
lazymushroom22 <33084619+lazymushroom22@users.noreply.github.com>
2020-05-04 17:29:20 +0300

    Add files via upload

---
+++ CarRentalProject/CarRentalProject/Controllers/CarController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRentalProject.ApplicationLogic.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalProject.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly iCarRepository carRepository;
+
+        public CarController(iCarRepository carRepository)
+        {
+            this.carRepository = carRepository;
+        }
+
+        public ActionResult GetCars()
+        {
+            var cars = carRepository.GetCars();
+            return View(cars);
+        }
+
+        public ActionResult GetCarsByBrand(string brand)
+        {
+            var cars = carRepository.GetCarsByBrand(brand);
+            return View(cars);
+        }
+
+        public ActionResult GetCarsByPriceLowerThan(double price)
+        {
+            var cars = carRepository.GetCarsByPriceLowerThan(price);
+            return View(cars);
+        }
+
+        public ActionResult SearchForCar(string name)
+        {
+            var cars = carRepository.SearchForCar(name);
+            return View(cars);
+        }
+    }
+}
---
+++ CarRentalProject/CarRentalProject/Controllers/TestimoniesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRentalProject.ApplicationLogic.Services;
+using CarRentalProject.DataAccess2;
+using CarRentalProject.Models.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalProject.Controllers
+{
+    public class TestimoniesController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly UserService userService;
+        public TestimoniesController(UserManager<IdentityUser> userManager, UserService userService)
+        {
+            this.userManager = userManager;
+            this.userService = userService;
+
+        }
+        // GET: Testimonies
+        public ActionResult Index()
+        {
+            var machines = TestimoniesRepository.GetTestimonyDescription();
+            return View();
+        }
+
+    }
+}
